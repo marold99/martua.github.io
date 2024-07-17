93bfd09380bd69f6f871aa72a16b874f8b7e1d8e
marold99 <86368215+marold99@users.noreply.github.com>
2024-07-18 03:11:38 +0700

    Update 1.0
    Update Test Sample Scrapping

--- README.md
+++
@@ -1,1 +0,0 @@
-# martua.github.io
---
+++ framerusercontent.com/sites/2Wv4lVIfy23BjKEhMEIflQ/chunk-5YUHMFEM.mjs
@@ -0,0 +1,10 @@
+function a(e,t){return{bodyClassName:"framer-body-augiA20Il",breakpoints:[{hash:"72rtr7",mediaQuery:"(min-width: 1466px)"},{hash:"umcu6w",mediaQuery:"(min-width: 1000px) and (max-width: 1465px)"},{hash:"z99zgz",mediaQuery:"(min-width: 840px) and (max-width: 999px)"},{hash:"v1qqas",mediaQuery:"(min-width: 654px) and (max-width: 839px)"},{hash:"1v997eb",mediaQuery:"(max-width: 653px)"}],customHTMLHeadStart:`<!-- Google tag (gtag.js) -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=G-QYWH8NE5K8"><\/script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QYWH8NE5K8');
+<\/script>`,description:"Daniel Sun - Your partner in digital design",elements:{BYeg4BelE:"connect",CXKaeNzEe:"hero",e2mBAIYqr:"exploratio",HsZZzRF5u:"areas",i2YXDLswt:"portfolio",Ss6VqI6V4:"story"},framerSearch:{index:!0},robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/6ob8fSvpRgFvfy4DOM8GbTe30gQ.png",title:"Daniel Sun - Your partner in digital design",viewport:"width=device-width"}}var o=1,s={exports:{default:{type:"function",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{a,o as b,s as c};
+//# sourceMappingURL=chunk-5YUHMFEM.mjs.map
